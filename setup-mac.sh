189353e9e6d13598b30c131a6dc1ac0455b79b15
Nando Pena <npena@mailgun.com>
2019-08-06 10:41:42 -0500

    Update files

---
+++ setup-mac.sh
@@ -0,0 +1,17 @@
+# Install Brew
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update / Install ZShell
+# brew install zsh
+
+# Oh My Zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Copy Zsh Config
+cp .zshrc ~/.zshrc
+
+# Copy Input RC - history up/down completion
+cp .inputrc ~/.inputrc
+
+# Copy Git Config
+cp .gitconfig ~/.gitconfig
